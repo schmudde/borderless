8fb99da06d410a9168e9f8f9b3686318fc00ff72
schmudde <schmudde@beyondthefra.me>
2016-08-01 22:41:09 -0400

    using samples

--- src/borderless/core.clj
+++ src/borderless/core.clj
@@ -4,9 +4,39 @@
 ;  (:require [overtone.live]))
 ;; Why can't I use require?!?
 
-(def PORT 12001)
+;; (volume 20)
 
-                                         ; start a server and create a client to talk with it
+;;;;;;;;;;;;;;;;;;;;;
+;; Audio           ;;
+;;;;;;;;;;;;;;;;;;;;;
+
+
+(def eh (sample "~/work/code/borderless/resources/audio/eh.aif"))
+
+(def noise-1 (sample "~/work/code/borderless/resources/audio/Test21-1.aif"))
+(def noise-2 (sample "~/work/code/borderless/resources/audio/Test21-2.aif"))
+(def noise-3 (sample "~/work/code/borderless/resources/audio/Test21-3.aif"))
+
+(def noise [noise-1 noise-2 noise-3])
+
+(def eh-buf (load-sample "resources/audio/eh.aif"))
+
+(defsynth reverb-on-left []
+  (let [dry (play-buf 1 eh-buf)
+    wet (free-verb dry 1)]
+    (out 0 [wet dry])))
+
+(defn testers!
+  "Side effect sound"
+  []
+  (demo (sin-osc)))
+
+;;;;;;;;;;;;;;;;;;;;;
+;; OSC Server      ;;
+;;;;;;;;;;;;;;;;;;;;;
+
+(def PORT 12000)
+
 (def server (osc-server PORT))
 (def client (osc-client "localhost" PORT))
 
@@ -17,17 +47,14 @@
   ;; stop listening and deallocate resources
   (osc-close server))
 
-
-(defn testers!
-  "Side effect sound"
-  []
-  (demo (sin-osc)))
+(def noise-chooser
+  (fn [] (rand-nth noise)))
 
 (defn person-entered []
-  (osc-handle server "/TSPS/personEntered" (fn [msg]
-                                             (testers!)
-                                             (println "Handler for /TSPS/personEntered: " msg))))
-
+  (osc-handle server "/TSPS/personEntered"
+              (fn [msg]
+                ((noise-chooser))
+                (println "Handler for /TSPS/personEntered: " msg))))
 
 (defn -main
   "I don't do a whole lot ... yet."
