7a6c3a15edc0d26d9b1e05d01241e3d313c5f479
schmudde <schmudde@beyondthefra.me>
2016-11-18 17:21:54 -0500

    refactoring instruments

--- src/borderless/sound.clj
+++ src/borderless/sound.clj
@@ -39,7 +39,7 @@
     (reset! person-sound (dissoc @person-sound pid))))
 
 ;;;;;;;;;;;;;;;;;;;;;
-;; Audio           ;;
+;; spec            ;;
 ;;;;;;;;;;;;;;;;;;;;;
 
 ;; VCA, VCO, and FX
@@ -66,16 +66,19 @@
    ::release 5.0
    ::gate 1})
 
-(defn tremelo-freq []
-  (fn [] (sin-osc:kr 0.5)))
-
 (s/fdef vowel-formant
         :args (s/cat :freq number? :eq-freq number? :q number?)
         :ret (s/fspec :args (s/cat :saw clojure.test/function? :freq number?)
                       :ret number?))
 
-(defn vowel-formant [freq eq-freq q]
-  (fn [] (resonz (saw freq) eq-freq q)))
+;;;;;;;;;;;;;;;;;;;;;
+;; Audio           ;;
+;;;;;;;;;;;;;;;;;;;;;
+
+(defn vowel-formant
+  "I EQ a narrow Q on a given frequency to shape a saw waveform of a certain pitch."
+  [pitch eq-freq q]
+  (fn [] (resonz (saw pitch) eq-freq q)))
 
 (defn synth-unit-layered [freq eq-freq q kr-mul]
   (let [[freq-a freq-b freq-c] eq-freq]
@@ -85,6 +88,7 @@
      ((vowel-formant (overtone.sc.ugen-collide/+ freq (sin-osc:kr (* 1.5 kr-mul))) freq-c q)))))
 
 (definst drone-eh
+  "a testing instrument, not used in runtime"
   [freq 100]
   (let [eq-freq      [530 1840 2480]
         q            0.1
@@ -93,63 +97,94 @@
         synth-unit-lpf (rlpf (* 0.5 synth-unit)
                                        (mouse-x 10 10000)
                                        (mouse-y 0.0001 0.9999))]
-
     synth-unit-lpf))
 
-(definst drone-aw-sus [freq 300
-                       amp (synth-defaults ::vca)
-                       verb (synth-defaults ::reverb)
-                       kr-mul (synth-defaults ::vco)
-                       attack (synth-defaults ::attack)
-                       sustain (synth-defaults ::sustain)
-                       release (synth-defaults ::release)
-                       gate (synth-defaults ::gate)]
-  (let [eq-freq      [570 840 2410]
-        q            0.1
-        synth-unit (synth-unit-layered freq eq-freq q 0.5)
-        synth-unit-env  (* (env-gen (asr attack sustain release) gate) synth-unit)
-        synth-unit-hpf  (hpf synth-unit-env 900)
-        synth-unit-lpf  (* amp (rlpf synth-unit-hpf 600 0.6))]
+(defn synth-filter-chain
+  "I shape a sound with high pass filters, resonante low pass filters, amplifiers, and reverb."
+  [synth-unit amp verb gate hpf-rlpf]
+  (let [attack (synth-defaults ::attack)
+        sustain (synth-defaults ::sustain)
+        release (synth-defaults ::release)
+        [hpf-freq rlpf-freq rlpf-q] hpf-rlpf]
 
-    (out 0 (free-verb synth-unit-lpf verb verb verb))
-    (out 1 (free-verb synth-unit-lpf verb verb verb))))
+    (-> synth-unit
+        (overtone.sc.ugen-collide/* (env-gen (asr attack sustain release) gate))
+        (hpf hpf-freq)
+        (rlpf rlpf-freq rlpf-q)
+        (overtone.sc.ugen-collide/* amp)
+        (free-verb verb verb verb))))
 
+(definst drone-aw-sus
+  "I make the 'aw' vowel sound at a given frequency.
+   I start/stop with the gate set to 1 or 0."
+  [freq   300
+   gate   (synth-defaults ::gate)
+   amp    (synth-defaults ::vca)
+   verb   (synth-defaults ::reverb)
+   kr-mul (synth-defaults ::vco)]
 
-(definst drone-ae-sus [freq 100 verb 1 kr-mul 25 amp 0.75
-                       attack 35.0 sustain 0.4 release 15.0 gate 1]
+    (let [kr-mul     (:value kr-mul)
+          eq-freq    [570 840 2410]
+          hpf-rlpf   [900 600 0.6]
+          q          0.1
+          synth-unit (synth-unit-layered freq eq-freq q kr-mul)]
 
-  (let [eq-freq      [270 2290 3010]
-        q            0.1
-        synth-unit (synth-unit-layered freq eq-freq q 0.5)
-        synth-unit-env  (* (env-gen (asr attack sustain release) gate) synth-unit)
-        synth-unit-hpf  (* amp (hpf synth-unit-env 600))
-        synth-unit-lpf  (rlpf synth-unit-hpf 8000 0.6)]
+        (synth-filter-chain synth-unit amp verb gate hpf-rlpf)))
 
-    (out 0 (free-verb synth-unit-lpf verb verb verb))
-    (out 1 (free-verb synth-unit-lpf verb verb verb))))
+(definst drone-ae-sus
+  "I make the 'ae' vowel sound at a given frequency.
+   I start/stop with the gate set to 1 or 0."
+  [freq   100
+   gate   (synth-defaults ::gate)
+   amp    (synth-defaults ::vca)
+   verb   (synth-defaults ::reverb)
+   kr-mul (synth-defaults ::vco)]
 
-(definst drone-eh-sus [freq 80 verb 1 kr-mul 25 amp 1
-                       attack 5.0 sustain 0.4 release 15.0 gate 1]
-  (let [eq-freq      [530 1840 2480]
-        q            0.1
-        synth-unit (synth-unit-layered freq eq-freq q 0.5)
-        synth-unit-env  (* (env-gen (asr attack sustain release) gate) synth-unit)
-        synth-unit-lpf  (rlpf synth-unit-env 750 0.9)]
+  (let [kr-mul     (:value kr-mul)
+        eq-freq    [270 2290 3010]
+        hpf-rlpf   [600 8000 0.6]
+        q          0.1
+        synth-unit (synth-unit-layered freq eq-freq q kr-mul)]
 
-    (out 0 (free-verb synth-unit-lpf verb verb verb))
-    (out 1 (free-verb synth-unit-lpf verb verb verb))))
+    (synth-filter-chain synth-unit amp verb gate hpf-rlpf)))
 
-(definst drone-oo-sus [freq 120 verb 1 kr-mul 25 amp 1
-                       attack 5.0 sustain 0.4 release 15.0 gate 1]
+(definst drone-eh-sus
+  "I make the 'eh' vowel sound at a given frequency.
+   I start/stop with the gate set to 1 or 0."
+  [freq   80
+   gate   (synth-defaults ::gate)
+   amp    (synth-defaults ::vca)
+   verb   (synth-defaults ::reverb)
+   kr-mul (synth-defaults ::vco)]
 
-  (let [eq-freq      [300 870 2240]
-        q            0.1
-        synth-unit (synth-unit-layered freq eq-freq q 0.5)
-        synth-unit-env  (* (env-gen (asr attack sustain release) gate) synth-unit)
-        synth-unit-lpf  (rlpf synth-unit-env 600 0.6)]
+  (let [kr-mul     (:value kr-mul)
+        eq-freq    [530 1840 2480]
+        hpf-rlpf   [0 750 0.9]
+        q          0.1
+        synth-unit (synth-unit-layered freq eq-freq q kr-mul)]
 
-    (out 0 (free-verb synth-unit-lpf verb verb verb))
-    (out 1 (free-verb synth-unit-lpf verb verb verb))))
+    (synth-filter-chain synth-unit amp verb gate hpf-rlpf)))
+
+(definst drone-oo-sus
+  "I make the 'oo' vowel sound at a given frequency.
+   I start/stop with the gate set to 1 or 0."
+  [freq   120
+   gate   (synth-defaults ::gate)
+   amp    (synth-defaults ::vca)
+   verb   (synth-defaults ::reverb)
+   kr-mul (synth-defaults ::vco)]
+
+  (let [kr-mul     (:value kr-mul)
+        eq-freq    [300 870 2240]
+        hpf-rlpf   [0 600 0.6]
+        q          0.1
+        synth-unit (synth-unit-layered freq eq-freq q kr-mul)]
+
+    (synth-filter-chain synth-unit amp verb gate hpf-rlpf)))
+
+;;;;;;;;;;;;;;;;;;;;;
+;; Control         ;;
+;;;;;;;;;;;;;;;;;;;;;
 
 (defn amplitude-mul
   "This is a quick 'n dirty function that lowers the amplitude over time. It was written to compensate the removal of reverb over time and the increase in the perceived loudness of a sound."
@@ -161,10 +196,6 @@
     (< age 1000) 0.5
     (>= age 1000) 0.4
     :else 1))
-
-;;;;;;;;;;;;;;;;;;;;;
-;; Control         ;;
-;;;;;;;;;;;;;;;;;;;;;
 
 (defn end-sound! [pid]
   (if (contains? @person-sound pid)
@@ -196,9 +227,8 @@
     3 (add-person-sound! pid "drone-oo-sus")
     "atom full: four people are tracked")))
 
-
 (defn control-sound
-  "Here's a fn which will take a val between 0 and 1, map it linearly to a value between 50 and 1000 and send the mapped value as the new frequency of foo:"
+  "I take a val between 0 and 1000+, map the value, and send it to the instrument's controller."
   [pid val]
   (let [verb-val  (scale-range val 0 1000 1 0.3)
         kr-val    (scale-range val 0 1000 25 0)
